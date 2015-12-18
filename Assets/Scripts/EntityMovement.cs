2f06d400f1d85a72303c2348a8f38543aedfc30c
myrovh <myrovh@yandex.com>
2015-12-18 18:56:52 +1100

    adjustments to grid and creation of basic script to find a walkable path

---
+++ Assets/Scripts/EntityMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Pathfinding;
+using System.Collections;
+
+public class EntityMovement : MonoBehaviour
+{
+    public Vector3 TargetPosition;
+    public Transform TargetObject;
+    private Seeker _seeker;
+
+	void Start ()
+	{
+	    _seeker = GetComponent<Seeker>();
+	    _seeker.pathCallback += OnPathComplete;
+
+	    _seeker.StartPath(transform.position, TargetObject.position);
+	}
+
+    void OnDisable()
+    {
+        _seeker.pathCallback -= OnPathComplete;
+    }
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Path Returned. Error: " + p.error);
+    }
+}
